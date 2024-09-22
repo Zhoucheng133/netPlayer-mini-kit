1ac8f8a2397bcc7d805ee25ac3dbc6b2cd35cb7a
周诚 <zhoucheng133@outlook.com>
2024-09-22 19:32:28 +0800

    添加信息变量存储

---
+++ lib/variables/data_var.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class DataVar extends GetxController{
+  RxString cover="".obs;
+  RxList lyric=[].obs;
+  RxInt line=0.obs;
+  RxString title="".obs;
+  RxString artist="".obs;
+}
