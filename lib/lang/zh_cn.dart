da4beb882ae9fc7fb60cc40f03c72963fc817c68
周诚 <zhoucheng133@outlook.com>
2024-09-23 21:39:01 +0800

    添加语言文件

---
+++ lib/lang/zh_cn.dart
@@ -0,0 +1,12 @@
+const Map<String, String> zhCN = {
+  'play/pause': '播放/暂停',
+  'skipPre': '上一首',
+  'skipNext': '下一首',
+  'playMode': '播放模式',
+  'loop': '列表循环',
+  'single': '单曲循环',
+  'shuffle': '随机播放',
+
+  'showShadow': '显示窗口阴影',
+  
+};
