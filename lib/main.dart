665a67d49638be28be82ed697083c5b1985ab1c1
周诚 <zhoucheng133@outlook.com>
2024-09-23 19:51:40 +0800

    改进响应速度

--- lib/main.dart
+++ lib/main.dart
@@ -77,209 +77,207 @@ class _MainAppState extends State<MainApp> with WindowListener {
             children: [
               SizedBox(
                 height: 25,
-                child: DragToMoveArea(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(child: Container()),
-                      CustomPopup(
-                        content: SizedBox(
-                          width: 150,
-                          height: 50,
-                          child: Column(
-                            children: [
-                              Obx(()=>
-                                Row(
-                                  children: [
-                                    Checkbox(
-                                      splashRadius: 0,
-                                      activeColor: Colors.blue,
-                                      value: s.showShadow.value, 
-                                      onChanged: (val) async {
-                                        s.showShadow.value=val??true;
-                                        if(!s.showShadow.value){
-                                          await windowManager.setAsFrameless();
-                                        }
-                                        windowManager.setHasShadow(s.showShadow.value);
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(child: DragToMoveArea(child: Container())),
+                    CustomPopup(
+                      content: SizedBox(
+                        width: 150,
+                        height: 50,
+                        child: Column(
+                          children: [
+                            Obx(()=>
+                              Row(
+                                children: [
+                                  Checkbox(
+                                    splashRadius: 0,
+                                    activeColor: Colors.blue,
+                                    value: s.showShadow.value, 
+                                    onChanged: (val) async {
+                                      s.showShadow.value=val??true;
+                                      if(!s.showShadow.value){
+                                        await windowManager.setAsFrameless();
                                       }
-                                    ),
-                                    const SizedBox(width: 10,),
-                                    GestureDetector(
-                                      onTap: () async {
-                                        s.showShadow.value=!s.showShadow.value;
-                                        if(!s.showShadow.value){
-                                          await windowManager.setAsFrameless();
-                                        }
-                                        windowManager.setHasShadow(s.showShadow.value);
-                                      },
-                                      child: const MouseRegion(
-                                        cursor: SystemMouseCursors.click,
-                                        child: Text('显示窗口阴影')
-                                      )
+                                      windowManager.setHasShadow(s.showShadow.value);
+                                    }
+                                  ),
+                                  const SizedBox(width: 10,),
+                                  GestureDetector(
+                                    onTap: () async {
+                                      s.showShadow.value=!s.showShadow.value;
+                                      if(!s.showShadow.value){
+                                        await windowManager.setAsFrameless();
+                                      }
+                                      windowManager.setHasShadow(s.showShadow.value);
+                                    },
+                                    child: const MouseRegion(
+                                      cursor: SystemMouseCursors.click,
+                                      child: Text('显示窗口阴影')
                                     )
-                                  ],
+                                  )
+                                ],
+                              )
+                            ),
+                            SliderTheme(
+                              data: SliderThemeData(
+                                thumbColor: Colors.blue,
+                                overlayColor: Colors.transparent,
+                                overlayShape: const RoundSliderOverlayShape(overlayRadius: 0.0),
+                                trackHeight: 2,
+                                thumbShape: const RoundSliderThumbShape(
+                                  enabledThumbRadius: 7,
+                                  elevation: 0,
+                                  pressedElevation: 0,
+                                ),
+                                activeTrackColor: Colors.blue[400],
+                                inactiveTrackColor: Colors.blue[200],
+                              ),
+                              child: Obx(()=>
+                                Slider(
+                                  value: (s.opacity.value)/255, 
+                                  onChanged: (val){
+                                    s.opacity.value=(val*255).toInt();
+                                  }
                                 )
                               ),
-                              SliderTheme(
-                                data: SliderThemeData(
-                                  thumbColor: Colors.blue,
-                                  overlayColor: Colors.transparent,
-                                  overlayShape: const RoundSliderOverlayShape(overlayRadius: 0.0),
-                                  trackHeight: 2,
-                                  thumbShape: const RoundSliderThumbShape(
-                                    enabledThumbRadius: 7,
-                                    elevation: 0,
-                                    pressedElevation: 0,
-                                  ),
-                                  activeTrackColor: Colors.blue[400],
-                                  inactiveTrackColor: Colors.blue[200],
-                                ),
-                                child: Obx(()=>
-                                  Slider(
-                                    value: (s.opacity.value)/255, 
-                                    onChanged: (val){
-                                      s.opacity.value=(val*255).toInt();
-                                    }
-                                  )
-                                ),
-                              ),
-                            ],
-                          )
-                        ),
-                        child: MouseRegion(
-                          cursor: SystemMouseCursors.click,
-                          onEnter: (_){
-                            setState(() {
-                              hoverOpacity=true;
-                            });
-                          },
-                          onExit: (_){
-                            setState(() {
-                              hoverOpacity=false;
-                            });
-                          },
-                          child: AnimatedContainer(
-                            duration: const Duration(milliseconds: 200),
-                            height: 25,
-                            width: 40,
-                            decoration: BoxDecoration(
-                              color: hoverOpacity ? const Color.fromARGB(255, 240, 240, 240) : const Color.fromARGB(0, 230, 230, 230)
                             ),
-                            child: Center(
-                              child: FaIcon(
-                                FontAwesomeIcons.droplet,
-                                size: 12,
-                                color: Colors.grey[700],
-                              ),
-                            ),
-                          ),
-                        ),
+                          ],
+                        )
                       ),
-                      GestureDetector(
-                        onTap: (){
+                      child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        onEnter: (_){
                           setState(() {
-                            alwaysOnTop=!alwaysOnTop;
+                            hoverOpacity=true;
                           });
-                          windowManager.setAlwaysOnTop(alwaysOnTop);
                         },
-                        child: MouseRegion(
-                          cursor: SystemMouseCursors.click,
-                          onEnter: (_){
-                            setState(() {
-                              hoverPin=true;
-                            });
-                          },
-                          onExit: (_){
-                            setState(() {
-                              hoverPin=false;
-                            });
-                          },
-                          child: AnimatedContainer(
-                            duration: const Duration(milliseconds: 200),
-                            height: 25,
-                            width: 40,
-                            decoration: BoxDecoration(
-                              color: hoverPin ? const Color.fromARGB(255, 240, 240, 240) : const Color.fromARGB(0, 230, 230, 230)
-                            ),
-                            child: Center(
-                              child: FaIcon(
-                                FontAwesomeIcons.thumbtack,
-                                size: 12,
-                                color: alwaysOnTop ? Colors.grey[700] : Colors.grey[400],
-                              ),
+                        onExit: (_){
+                          setState(() {
+                            hoverOpacity=false;
+                          });
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          height: 25,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            color: hoverOpacity ? const Color.fromARGB(255, 240, 240, 240) : const Color.fromARGB(0, 230, 230, 230)
+                          ),
+                          child: Center(
+                            child: FaIcon(
+                              FontAwesomeIcons.droplet,
+                              size: 12,
+                              color: Colors.grey[700],
                             ),
                           ),
                         ),
                       ),
-                      GestureDetector(
-                        onTap: (){
-                          // TODO 切换歌词
+                    ),
+                    GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          alwaysOnTop=!alwaysOnTop;
+                        });
+                        windowManager.setAlwaysOnTop(alwaysOnTop);
+                      },
+                      child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        onEnter: (_){
+                          setState(() {
+                            hoverPin=true;
+                          });
                         },
-                        child: MouseRegion(
-                          cursor: SystemMouseCursors.click,
-                          onEnter: (_){
-                            setState(() {
-                              hoverLyric=true;
-                            });
-                          },
-                          onExit: (_){
-                            setState(() {
-                              hoverLyric=false;
-                            });
-                          },
-                          child: AnimatedContainer(
-                            duration: const Duration(milliseconds: 200),
-                            decoration: BoxDecoration(
-                              color: hoverLyric ? const Color.fromARGB(255, 240, 240, 240) : const Color.fromARGB(0, 230, 230, 230)
-                            ),
-                            height: 25,
-                            width: 40,
-                            child: Center(
-                              child: Icon(
-                                Icons.lyrics_rounded,
-                                size: 14,
-                                color: Colors.grey[700],
-                              ),
+                        onExit: (_){
+                          setState(() {
+                            hoverPin=false;
+                          });
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          height: 25,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            color: hoverPin ? const Color.fromARGB(255, 240, 240, 240) : const Color.fromARGB(0, 230, 230, 230)
+                          ),
+                          child: Center(
+                            child: FaIcon(
+                              FontAwesomeIcons.thumbtack,
+                              size: 12,
+                              color: alwaysOnTop ? Colors.grey[700] : Colors.grey[400],
                             ),
                           ),
                         ),
                       ),
-                      GestureDetector(
-                        onTap: (){
-                          windowManager.close();
-                          // TODO 关闭
+                    ),
+                    GestureDetector(
+                      onTap: (){
+                        // TODO 切换歌词
+                      },
+                      child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        onEnter: (_){
+                          setState(() {
+                            hoverLyric=true;
+                          });
                         },
-                        child: MouseRegion(
-                          cursor: SystemMouseCursors.click,
-                          onEnter: (_){
-                            setState(() {
-                              hoverClose=true;
-                            });
-                          },
-                          onExit: (_){
-                            setState(() {
-                              hoverClose=false;
-                            });
-                          },
-                          child: AnimatedContainer(
-                            duration: const Duration(milliseconds: 200),
-                            decoration: BoxDecoration(
-                              color: hoverClose ? const Color.fromARGB(255, 210, 0, 0) : const Color.fromARGB(0, 230, 230, 230)
-                            ),
-                            height: 25,
-                            width: 40,
-                            child: Center(
-                              child: FaIcon(
-                                FontAwesomeIcons.xmark,
-                                size: 14,
-                                color: hoverClose ? Colors.white : Colors.grey[700],
-                              ),
+                        onExit: (_){
+                          setState(() {
+                            hoverLyric=false;
+                          });
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          decoration: BoxDecoration(
+                            color: hoverLyric ? const Color.fromARGB(255, 240, 240, 240) : const Color.fromARGB(0, 230, 230, 230)
+                          ),
+                          height: 25,
+                          width: 40,
+                          child: Center(
+                            child: Icon(
+                              Icons.lyrics_rounded,
+                              size: 14,
+                              color: Colors.grey[700],
                             ),
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    GestureDetector(
+                      onTap: (){
+                        windowManager.close();
+                        // TODO 关闭
+                      },
+                      child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        onEnter: (_){
+                          setState(() {
+                            hoverClose=true;
+                          });
+                        },
+                        onExit: (_){
+                          setState(() {
+                            hoverClose=false;
+                          });
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          decoration: BoxDecoration(
+                            color: hoverClose ? const Color.fromARGB(255, 210, 0, 0) : const Color.fromARGB(0, 230, 230, 230)
+                          ),
+                          height: 25,
+                          width: 40,
+                          child: Center(
+                            child: FaIcon(
+                              FontAwesomeIcons.xmark,
+                              size: 14,
+                              color: hoverClose ? Colors.white : Colors.grey[700],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Expanded(child: MainView(ws: ws,))
